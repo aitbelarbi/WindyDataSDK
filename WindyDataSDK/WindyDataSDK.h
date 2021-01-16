d13b06785f8b193630f7d191fcfe89e16f5f41ff
Mohamed Amine AIT BELARBI <ait.belarbi@gmail.col>
2021-01-16 20:44:40 +0100

    Initial Commit

---
+++ WindyDataSDK/WindyDataSDK.h
@@ -0,0 +1,18 @@
+//
+//  WindyDataSDK.h
+//  WindyDataSDK
+//
+//  Created by ait belarbi mohamed amine on 16/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WindyDataSDK.
+FOUNDATION_EXPORT double WindyDataSDKVersionNumber;
+
+//! Project version string for WindyDataSDK.
+FOUNDATION_EXPORT const unsigned char WindyDataSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WindyDataSDK/PublicHeader.h>
+
+
---
+++ WindyDataSDKTests/WindyDataSDKTests.swift
@@ -0,0 +1,33 @@
+//
+//  WindyDataSDKTests.swift
+//  WindyDataSDKTests
+//
+//  Created by ait belarbi mohamed amine on 16/01/2021.
+//
+
+import XCTest
+@testable import WindyDataSDK
+
+class WindyDataSDKTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
